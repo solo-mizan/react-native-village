018f94c5a62c58d62d9bca0cb79632b9dbf4fa6c
solo-mizan <mizanbd.eee@gmail.com>
2022-04-21 23:38:54 +0600

    react-router-dom installed and components are added

---
+++ src/Pages/Home/Slider/Slider.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Slider.css';
+
+const Slider = () => {
+    return (
+        <div>
+            <h2>Slider here</h2>
+        </div>
+    );
+};
+
+export default Slider;
